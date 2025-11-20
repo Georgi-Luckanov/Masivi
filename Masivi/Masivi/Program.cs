bb8aa5e26ad44a9d5cd968a439fda53023a5c4ce
Georgi-Luckanov <et.georgi.l@eduiteh.eu>
2025-11-20 11:21:10 +0200

    Masivi
    Masivi uor

---
+++ Masivi/Masivi/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int[]ivo = new int[7];
+int max = int.MinValue;
+int min = int.MaxValue;
+int a;
+ivo[0] = int.Parse(Console.ReadLine());
+int sum = 0;
+for (int i = 0; i < ivo.Length; i++)
+{
+    ivo[i] = int.Parse(Console.ReadLine());
+    sum += ivo[i];
+    if (ivo[i] > max)
+    {
+        max = ivo[i];
+    }
+    if (ivo[i] < min)
+    {
+        min = ivo[i];
+    }
+}
+Console.WriteLine(sum);
+Console.WriteLine(max + " " + min);
